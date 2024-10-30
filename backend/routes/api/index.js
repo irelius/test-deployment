3b40ce6f3e28ee5c95e50718cc2ee233b5e14e58
Togar-GitHub <togar_unix@Togar-Laptop>
2024-10-30 13:47:17 -0700

    Add an API router

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,3 +2,6 @@ const router = require('express').Router();
 
 module.exports = router;
 
+router.post('/test', function(req, res) {
+    res.json({ requestBody: req.body });
+  });
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-<<<<<<< HEAD
 // Add a XSRF-TOKEN cookie
 router.get("/api/csrf/restore", (req, res) => {
   const csrfToken = req.csrfToken();
@@ -15,23 +14,10 @@ const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
 
-=======
->>>>>>> 93b18c805030c184f280ef6756c292c792b6af7d
 // router.get('/hello/world', function(req, res) {
 //   res.cookie('XSRF-TOKEN', req.csrfToken());
 //   res.send('Hello World!');
 // });
 
-// backend/routes/index.js
-// ...
-// Add a XSRF-TOKEN cookie
-router.get("/api/csrf/restore", (req, res) => {
-    const csrfToken = req.csrfToken();
-    res.cookie("XSRF-TOKEN", csrfToken);
-    res.status(200).json({
-      'XSRF-Token': csrfToken
-    });
-  });
-  // ...
   
 module.exports = router;
