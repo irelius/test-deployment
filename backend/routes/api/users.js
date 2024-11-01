f791475d5b8919b7277d0d764e0d8c7974a46e3e
awright222 <alex.gr.wright@gmail.com>
2024-11-01 17:58:20 -0600

    users.js updated with reviews, get userId, login

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { User } = require('../../db/models');
+const { User } = require('../../db/models'); 
 
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
@@ -30,27 +30,83 @@ const validateSignup = [
   handleValidationErrors
 ];
 
+//Get Reviews of current user
+router.get(
+  '/users/:userId/reviews',
+  requireAuth,
+  async (req, res) => {
+    const userReviews = await Review.findAll({ where: { userId: req.params.userId }});
+
+    if (!userReviews) {
+      return res.status(400).json({ message: 'No reviews found for this user' });
+    }
+
+    return res.json({ reviews: userReviews })
+  }
+)
+
+
+
 // Sign up
 router.post(
-    '/',
+    '/users/new',   //path
     validateSignup,
     async (req, res) => {
-      const { email, password, username } = req.body;
+      const { email, password, username, firstName, lastName } = req.body;
       const hashedPassword = bcrypt.hashSync(password);
-      const user = await User.create({ email, username, hashedPassword });
+      const user = await User.create({ email, username, hashedPassword, firstName, lastName });
   
       const safeUser = {
         id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
         email: user.email,
         username: user.username,
       };
   
       await setTokenCookie(res, safeUser);
   
-      return res.json({
-        user: safeUser
-      });
+      return res.json({ user: safeUser });
     }
   );
+
+//Get User Id
+router.get('/users/:userId', requireAuth, async (req, res) => {
+  const user = await User.findByPk(req.params.userId, {
+    attributes: { exclude: ['hashedPassword']}
+  });
+
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+
+  return res.json(user);
+})
+
+
+
+  // Login
+router.post(
+  '/users/login',  
+  async (req, res) => {
+  const { credential, password } = req.body;   
+  const user = await User.findOne({ where: { credential } });  
+
+  if (!user || !bcrypt.compareSync(password, user.hashedPassword)) {
+    return res.status(401).json({ message: 'Invalid user name or password'});
+  }
+
+  const safeUser = {
+    id: user.id, 
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: user.email, 
+    username: user.username };
+
+  await setTokenCookie(res, safeUser);
+
+  return res.json({ user: safeUser });
+
+});
 
 module.exports = router;
