77dab2317c7c8a50cb9ff3f1cd670d35a302cbcf
Togar-GitHub <togar_unix@Togar-Laptop>
2024-11-04 11:12:45 -0800

    some updates

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,14 +4,18 @@ const usersRouter = require('./users.js');
 const spotRouter = require('./spots.js');
 const reviewRouter = require('./reviews.js');
 const bookingRouter = require('./bookings.js')
-const { restoreUser } = require('../../utils/auth.js');
+const { restoreUser, requireAuth } = require('../../utils/auth.js');
+
+router.get('/test', requireAuth, (req, res) => {
+  res.json ({ message: 'success' })
+})
 
 // Connect restoreUser middleware to the API router
   // If current user session is valid, set req.user to the user in the database
   // If current user session is not valid, set req.user to null
   router.use(restoreUser);
 
-  router.use('/session', sessionRouter);
+  router.use('/session', sessionRouter);    // to login and logout into the session
   router.use('/users', usersRouter);
   router.use('/spots', spotRouter);
   router.use('/reviews', reviewRouter);
