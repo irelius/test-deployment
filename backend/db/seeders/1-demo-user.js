d634d9ffb6cb2407efcba3721d3ca3cedca9728e
irelius <kihong711@hotmail.com>
2024-11-06 14:05:50 -0800

    stuff

--- backend/db/seeders/1-demo-user.js
+++ backend/db/seeders/1-demo-user.js
@@ -9,6 +9,7 @@ let options = {};
 if (process.env.NODE_ENV === 'production') {
     options.schema = process.env.SCHEMA;  // define your schema in options object
 }
+options.tableName = 'Users';
 
 module.exports = {
     async up(queryInterface, Sequelize) {
@@ -32,10 +33,6 @@ module.exports = {
     },
 
     async down(queryInterface, Sequelize) {
-        options.tableName = 'Users';
-        const Op = Sequelize.Op;
-        return queryInterface.bulkDelete(options, {
-            username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
-        }, {});
+        await queryInterface.bulkDelete(options, {}, {})
     }
 };
