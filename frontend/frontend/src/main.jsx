fb4fe841294ee7314ed9086b0883c7e0d2bf4d68
awright222 <alex.gr.wright@gmail.com>
2024-11-22 10:11:09 -0700

    auth linking

--- frontend/frontend/src/main.jsx
+++ frontend/frontend/src/main.jsx
@@ -5,7 +5,16 @@ import App from './App';
 import './index.css';
 import configureStore from './store';
 
+import { restoreCSRF, csrfFetch } from './store/csrf';
+
 const store = configureStore();
+
+if (import.meta.env.MODE !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
