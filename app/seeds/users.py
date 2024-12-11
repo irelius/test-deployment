2f500619975e0ba2e6acd936663c2abb05fb23bd
irelius <kihong711@hotmail.com>
2024-12-10 20:51:43 -0800

    reset migration and models to be different

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -5,11 +5,11 @@ from sqlalchemy.sql import text
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
-        username='Demo', email='demo@aa.io', password='password')
+        username='Demo', email='demo@aa.io', test="this is the test column", password='password')
     marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
+        username='marnie', email='marnie@aa.io', test="this is the test column", password='password')
     bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
+        username='bobbie', email='bobbie@aa.io', test="this is the test column", password='password')
 
     db.session.add(demo)
     db.session.add(marnie)
